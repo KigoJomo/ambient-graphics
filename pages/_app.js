adf5696aca4b414cb6ddbdca5a602eaee688aa03
KigoJomo <kigojomo@gmail.com>
2024-07-28 04:37:25 +0300

    Routes Updated

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import "../app/globals.css"; // Ensure this path points to your global CSS file
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
