4bd3bf14ff2e36e279166f38dfc3258382d4c034
KigoJomo <kigojomo@gmail.com>
2024-08-17 01:05:13 +0300

    Home-About

--- app/components/Home/HomeAbout.js
+++ app/components/Home/HomeAbout.js
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import ScrollAnimationWrapper from '../ScrollAnimationWrapper'
 import SplitWord from '../SplitWord'
+import RoundLink from '../RoundLink'
+import TypewriterComponent from '../TypewriterComponent'
 
 const HomeAbout = () => {
   return (
     <section className="h-fit flex flex-col gap-0">
       <div className="top w-full md:h-[65vh] flex flex-col md:flex-row gap-4 relative">
-
         <div className="image w-auto absolute opacity-5 -right-36 -top-12 -z-10">
           <Image
             alt="ambient graphics black panther"
@@ -16,7 +17,12 @@ const HomeAbout = () => {
           />
         </div>
 
-        <ScrollAnimationWrapper className={'image flex-shrink-0 w-3/5 md:w-auto md:h-full aspect-[3/4]'}>
+        <ScrollAnimationWrapper
+          className={
+            'image flex-shrink-0 w-3/5 md:w-auto md:h-full aspect-[3/4]'
+          }
+          duration={2.5}
+        >
           <Image
             alt="ambient graphics artists"
             src="/images/artists.webp"
@@ -26,16 +32,61 @@ const HomeAbout = () => {
         </ScrollAnimationWrapper>
 
         <div className="w-full flex flex-col md:justify-between gap-8 md:h-full">
-          <p className="md:w-2/5">
-            Explore a world of captivating art. Uncover the talented artists
-            behind our diverse collection of paintings, drawings, and more. Learn
-            about our dedication to showcasing unique and inspiring visual
-            expressions.
-          </p>
-          <SplitWord className="w-full justify-end md:justify-start" word={'love'} />
+          <ScrollAnimationWrapper duration={3} className={"md:w-2/5"}>
+            <p className="">
+              Explore a world of captivating art. Uncover the talented artists
+              behind our diverse collection of paintings, drawings, and more.
+              Learn about our dedication to showcasing unique and inspiring visual
+              expressions.
+            </p>
+          </ScrollAnimationWrapper>
+          <ScrollAnimationWrapper variant="slideInRight">
+            <SplitWord
+              className="w-full justify-end md:justify-start"
+              word={'love'}
+            />
+          </ScrollAnimationWrapper>
         </div>
       </div>
-      {/* .bottom */}
+
+      <div className="bottom flex flex-col md:flex-row gap-8 md:gap-4 md:justify-end relative">
+        <div className="image w-full md:w-auto absolute opacity-5 -left-32 top-[45%] md:top-0 -z-10">
+          <Image
+            alt="ambient graphics black panther"
+            src="/images/panther_emblem_brown.webp"
+            width={500}
+            height={500}
+          />
+        </div>
+
+        <ScrollAnimationWrapper variant="slideInLeft" className={"flex-shrink-0"}>
+          <SplitWord word={'art'} className="" />
+        </ScrollAnimationWrapper>
+
+        <div className="w-full md:w-1/3 md:mt-6 flex justify-between md:justify-end relative">
+          <div className="md:absolute right-[125%] bottom-[30%]">
+            <RoundLink href={'/spotlight'} text={'spotlight'} />
+          </div>
+
+          <div className="image-text w-3/5 md:w-full flex flex-col gap-4 md:flex-row-reverse relative">
+            <ScrollAnimationWrapper className="image w-full aspect-[3/4]">
+              <Image
+                alt="ambient graphics artists"
+                width={750}
+                height={750}
+                src="/images/artists2.webp"
+              />
+            </ScrollAnimationWrapper>
+
+            <TypewriterComponent
+              text={
+                'Join us in celebrating timeless artistry and unmatched creativity.'
+              }
+              className="text-right md:absolute right-[105%] bottom-0 md:w-[25vw]"
+            />
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
--- app/components/TypewriterComponent.js
+++ app/components/TypewriterComponent.js
@@ -1,19 +1,50 @@
-'use client'
-import { delay } from 'framer-motion'
-import React from 'react'
-import Typewriter from 'typewriter-effect'
+"use client";
+import { useRef, useEffect } from "react";
+import Typewriter from "typewriter-effect/dist/core";
+import React from "react";
 
-const TypewriterComponent = ({ text }) => {
-  return (
-    <Typewriter
-      options={{
-        delay: 30,
-      }}
-      onInit={(typewriter) => {
-        typewriter.typeString(text).pauseFor(500).start()
-      }}
-    />
-  )
-}
+const TypewriterComponent = ({ textLevel = "p", text, className = " " }) => {
+  const typewriterRef = useRef(null);
+  const observerRef = useRef(null);
 
-export default TypewriterComponent
+  const stringText = text.toString();
+
+  useEffect(() => {
+    const observerCallback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const typewriter = new Typewriter(typewriterRef.current, {
+            loop: false,
+            delay: 60,
+          });
+          typewriter.pauseFor(1500).typeString(stringText).pauseFor(1000).start();
+          // Stop observing after starting the animation
+          observerRef.current.disconnect();
+        }
+      });
+    };
+
+    observerRef.current = new IntersectionObserver(observerCallback, {
+      threshold: 0.1, // Trigger when 10% of the component is visible
+    });
+
+    if (typewriterRef.current) {
+      observerRef.current.observe(typewriterRef.current);
+    }
+
+    return () => {
+      // Cleanup observer on component unmount
+      if (observerRef.current) {
+        observerRef.current.disconnect();
+      }
+    };
+  }, [stringText]);
+
+  return React.createElement(
+    textLevel,
+    { ref: typewriterRef, className: className },
+    ""
+  );
+};
+
+export default TypewriterComponent;
