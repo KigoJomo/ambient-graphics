ee14fcc13d59f9344057d8893f49aac9edd780ef
KigoJomo <kigojomo@gmail.com>
2024-08-31 20:08:11 +0300

    Catalog UI update

--- app/catalog/page.js
+++ app/catalog/page.js
@@ -8,19 +8,19 @@ const collections = [
   {
     title: 'Paintings',
     description: 'Explore our collection of beautiful paintings.',
-    coverImage: '/images/paintings/painting1.webp',
+    coverImage: ['/images/paintings/painting1.webp'],
     slug: 'paintings',
   },
   {
     title: 'Pencil Sketches',
     description: 'Discover intricate pencil sketches.',
-    coverImage: '/images/sketches/sketch1.webp',
+    coverImage: ['/images/sketches/sketch1.webp'],
     slug: 'pencil-sketches',
   },
   {
     title: 'Digital Art',
     description: 'Modern and abstract digital art pieces.',
-    coverImage: '/images/digital-art/digital-art1.webp',
+    coverImage: ['/images/digital-art/digital-art1.webp'],
     slug: 'digital-art',
   },
   // Add more collections here
@@ -35,17 +35,20 @@ export default function CatalogPage() {
           key={collection.slug}
           className="collection w-full bg-transparent flex flex-col md:flex-row gap-4 md:py-8"
         >
-          <ScrollAnimationWrapper className={`w-full md:w-3/5 aspect-video md:aspect-[21/9] ${index % 2 && 'md:order-2'} overflow-hidden flex`}>
+
+          <ScrollAnimationWrapper className={`images w-full md:w-3/5 aspect-video md:aspect-[21/9] md:order-2 overflow-hidden flex`}>
             <Image
-              src={collection.coverImage}
+              src={collection.coverImage[0]}
               alt={collection.title}
               className="w-full hover:scale-110"
               width={500}
               height={500}
             />
           </ScrollAnimationWrapper>
-          <div className="w-full md:w-2/5 flex flex-col gap-4 flex-grow-0 flex-shrink-0">
-            <ScrollAnimationWrapper className={'-mt-11 md:mt-0'} variant='slideInBottom' duration={0.5}>
+
+          <div className={`details w-full md:w-2/5 flex flex-col gap-4 flex-grow-0 flex-shrink-0`}>
+
+            <ScrollAnimationWrapper className={`-mt-11 md:mt-0`} variant='slideInBottom' duration={0.5}>
               <h2 className="text-5xl md:text-7xl mix-blend-exclusion md:tracking-wider md:text-nowrap">{collection.title}</h2>
             </ScrollAnimationWrapper>
 
@@ -56,7 +59,9 @@ export default function CatalogPage() {
             <ScrollAnimationWrapper variant='slideInBottom' duration={1.5} className="w-full flex justify-end">
               <RoundLink href={`/catalog/${collection.slug}`} text='explore'  />
             </ScrollAnimationWrapper>
+
           </div>
+
         </div>
       ))}
     </section>
