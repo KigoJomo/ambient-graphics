97b52e6f83ad0d624ac28a861fd4c0cdc67671c8
KigoJomo <kigojomo@gmail.com>
2024-09-03 03:02:03 +0300

    Image Loader

--- app/components/ImageOverlayModal.js
+++ app/components/ImageOverlayModal.js
@@ -1,9 +1,11 @@
-// app/components/ImageOverlayModal.js
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import ScrollAnimationWrapper from './ScrollAnimationWrapper'
+import Loader from './Loader'
 
 const ImageOverlayModal = ({ imageSrc, imageAlt, onClose }) => {
+  const [isLoading, setIsLoading] = useState(true) // State to manage loading
+
   return (
     <ScrollAnimationWrapper
       duration={0.3}
@@ -12,18 +14,26 @@ const ImageOverlayModal = ({ imageSrc, imageAlt, onClose }) => {
     >
       <button
         onClick={onClose}
-        className="absolute top-4 right-4 text-white text-4xl font-bold cursor-pointer font-lato"
+        className="absolute top-4 right-4 text-white text-4xl font-bold cursor-pointer font-lato z-50"
       >
         &times;
       </button>
-      <div className="w-full h-full max-w-4xl">
+
+      <div className="relative w-full h-full max-w-4xl flex items-center justify-center">
+        {/* Loader */}
+        {isLoading && (
+          <div className="absolute inset-0 flex items-center justify-center z-10">
+            <Loader /> {/* Replace with your actual Loader component */}
+          </div>
+        )}
+
         <Image
           src={imageSrc}
           alt={imageAlt}
-          // layout="responsive"
           width={800}
           height={600}
           className="object-contain h-full"
+          onLoadingComplete={() => setIsLoading(false)} // Hide loader on load completion
         />
       </div>
     </ScrollAnimationWrapper>
